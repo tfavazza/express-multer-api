341169adc991d24a147ef47db0652454fcfa5f01
tfavazza <tara.favazza@gmail.com>
2016-07-08 15:28:48 -0400

    Refactor into an upload module
    This module is useable from both command line and express api server

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -3,62 +3,64 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const crypto = require('crypto');
+//const crypto = require('crypto');
 
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
+// const fileType = require('file-type');
+//const AWS = require('aws-sdk');
+const uploader = require('../lib/aws-s3-upload.js');
 
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+// const s3 = new AWS.S3({
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   },
+// });
 
 
 let filename = process.argv[2] || '';
 
-const mimeType = (data) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream'
-  }, fileType(data));
-};
+// const prepareFile = (data) => {
+//   return Object.assign({
+//     data,
+//     ext: 'bin',
+//     mime: 'application/octet-stream'
+//   }, fileType(data));
+// };
 
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
+// const randomHexString = (length) => {
+//   return new Promise((resolve, reject) => {
+//     crypto.randomBytes(length, (error, buffer) => {
+//       if (error) {
+//         reject(error);
+//       }
+//       resolve(buffer.toString('hex'));
+//     });
+//   });
+// };
 
-const awsUpload = (file) => {
-  return randomHexString(16)
-    .then((filename) => {
-      let dir = new Date().toISOString().split('T')[0];
-      return {
-        ACL: "public-read",
-        Body: file.data,
-        Bucket: 'tarawdibucket',
-        ContentType: file.mime,
-        Key: `${dir}/${filename}.${file.ext}`
-      };
-    }).
-    then((options) => {
-      return new Promise((resolve, reject) => {
-        s3.upload(options, (error, data) => {
-          if (error) {
-            reject(error);
-          }
-          resolve(data);
-        });
-      });
-    });
-};
+// const awsUpload = (file) => {
+//   return randomHexString(16)
+//     .then((filename) => {
+//       let dir = new Date().toISOString().split('T')[0];
+//       return {
+//         ACL: "public-read",
+//         Body: file.data,
+//         Bucket: 'tarawdibucket',
+//         ContentType: file.mime,
+//         Key: `${dir}/${filename}.${file.ext}`
+//       };
+//     }).
+//     then((options) => {
+//       return new Promise((resolve, reject) => {
+//         s3.upload(options, (error, data) => {
+//           if (error) {
+//             reject(error);
+//           }
+//           resolve(data);
+//         });
+//       });
+//     });
+// };
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -72,11 +74,7 @@ const readFile = (filename) => {
 };
 
 readFile(filename)
-  .then((data) => {
-    let file = mimeType(data); //build up a file object because S3 expects it,
-    //and some more stuff
-    file.data = data;
-    return file;
-  }).then(awsUpload) //upload to aws
+  .then(uploader.prepareFile)
+  .then(uploader.awsUpload) //upload to aws
   .then(console.log)
   .catch(console.error);
