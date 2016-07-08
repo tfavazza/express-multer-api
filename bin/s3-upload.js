a7142691e852147d9f3c336a648fab0958f8026c
tfavazza <tara.favazza@gmail.com>
2016-07-08 13:09:35 -0400

    determine file/mime type and start AWS upload function
    Use the file-type module to determine content-type of the upload.
    Configured options for our upload based on our bucket-name.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,8 +1,27 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 let filename = process.argv[2] || '';
+
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream'
+  }, fileType(data));
+};
+
+const awsUpload = (file) => {
+  const options = {
+    ACL: "public-read-write",
+    Body: file.data,
+    Bucket: 'tarawdibucket',
+    ContentType: file.mime,
+    Key: `test/test.${file.ext}`
+  };
+  return Promise.resolve(options);
+};
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -16,7 +35,11 @@ const readFile = (filename) => {
 };
 
 readFile(filename)
-.then((data) =>{
-    console.log(`${filename} is ${data.length} bytes long.`);
-})
+.then((data) => {
+  let file = mimeType(data); //build up a file object because S3 expects it,
+  //and some more stuff
+  file.data = data;
+  return file;
+}).then(awsUpload) //upload to aws
+.then(console.log)
 .catch(console.error);
