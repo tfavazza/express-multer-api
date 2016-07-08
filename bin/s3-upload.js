846c4ee8301bed7a71008a640c7ed9e0cadfab38
tfavazza <tara.favazza@gmail.com>
2016-07-08 14:41:46 -0400

    use crypto to generate better filenames
    crypto with a dated folder reduce the chance of name collision when
    users upload files at the same time.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -3,6 +3,8 @@
 require('dotenv').config();
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
@@ -23,23 +25,39 @@ const mimeType = (data) => {
   }, fileType(data));
 };
 
-const awsUpload = (file) => {
-  const options = {
-    ACL: "public-read-write",
-    Body: file.data,
-    Bucket: 'tarawdibucket',
-    ContentType: file.mime,
-    Key: `test/test.${file.ext}`
-  };
+const randomHexString = (length) => {
   return new Promise((resolve, reject) => {
-    s3.upload(options, (error, data) => {
+    crypto.randomBytes(length, (error, buffer) => {
       if (error) {
         reject(error);
       }
-
-      resolve(data);
+      resolve(buffer.toString('hex'));
     });
   });
+};
+
+const awsUpload = (file) => {
+  return randomHexString(16)
+    .then((filename) => {
+      let dir = new Date().toISOString().split('T')[0];
+      return {
+        ACL: "public-read",
+        Body: file.data,
+        Bucket: 'tarawdibucket',
+        ContentType: file.mime,
+        Key: `${dir}/${filename}.${file.ext}`
+      };
+    }).
+    then((options) => {
+      return new Promise((resolve, reject) => {
+        s3.upload(options, (error, data) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(data);
+        });
+      });
+    });
 };
 
 const readFile = (filename) => {
